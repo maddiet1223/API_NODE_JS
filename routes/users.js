30d1fa9fa2b88a8e0b2d420dfc35868b0a86b8e0
kiranmmu <mkiranala@gmail.com>
2023-10-04 12:31:29 -0500

    changes to the code

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 import express from 'express';
+import { createUser, deleteUser, editUser, getUser, updateUser } from '../controllers/users.js';
+
 
 const router = express.Router();
 
-router.get('/users',(req,res)=>{
-    res.send('Hello');
-})
+
+
+//all routes in here are starting with /users
+router.get('/',getUser)
+
+router.post('/',createUser);
+
+router.get('/:id',editUser);
+
+router.delete('/:id',deleteUser);
+
+router.patch('/:id',updateUser)
 
 export default router;
